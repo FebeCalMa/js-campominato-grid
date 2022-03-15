4c916853f098aeff480fa8eae57373206829c122
Febe Calderon <samcalma1@gmail.com>
2022-03-15 16:18:34 +0100

    contaniner e quandri

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("JS OK!");
+
+//Stampare i numeri da 1 a 100
+const blocksContainer = document.getElementById("blocks");
+
+for (let i = 1; i <= 100; i++) {
+  let message = i;
+
+  let classToAdd = "";
+
+  console.log(message);
+
+  const element = `<div class= "block ${message}">${message}</div>`;
+
+  blocksContainer.innerHTML += element;
+}
